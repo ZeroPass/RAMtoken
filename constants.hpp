a335fb4c6cc3cae599c1ce70c9c04100edb273d9
smlu <smlu@s5.net>
2018-08-14 01:27:07 +0200

    Add EOS_SYMBOL macro

--- constants.hpp
+++ constants.hpp
@@ -6,38 +6,37 @@
 
 
 #define RAM_SYMBOL S(0, RAM)
+#define EOS_SYMBOL CORE_SYMBOL
 #define EOS_TOKEN_CONTRACT N(eosio.token)
 #define RAM_TOKEN_CONTRACT N(ram.token)
 
 static constexpr int32_t min_ttl = 60; // 1min
 static constexpr int32_t infinite_ttl = -1;
 
-inline eosio::extended_symbol eos_symbol() {
-    return eosio::extended_symbol(CORE_SYMBOL, EOS_TOKEN_CONTRACT);
+static eosio::extended_symbol eos_symbol() {
+    return eosio::extended_symbol(EOS_SYMBOL, EOS_TOKEN_CONTRACT);
 }
 
-inline eosio::extended_asset eos_asset(eosio::asset asset) 
+static eosio::extended_asset eos_asset(eosio::asset asset) 
 {
-    eosio_assert(asset.symbol == CORE_SYMBOL, "Invalid EOS asset!");
+    eosio_assert(asset.symbol == EOS_SYMBOL, "Invalid EOS asset!");
     return eosio::extended_asset(asset, EOS_TOKEN_CONTRACT);
 }
 
-inline eosio::extended_symbol ram_symbol() {
+static eosio::extended_symbol ram_symbol() {
     return eosio::extended_symbol(RAM_SYMBOL, EOS_TOKEN_CONTRACT);
 }
 
-inline eosio::extended_asset ram_asset(eosio::asset asset) 
+static eosio::extended_asset ram_asset(eosio::asset asset) 
 {
     eosio_assert(asset.symbol == RAM_SYMBOL, "Invalid RAM asset!");
     return eosio::extended_asset(asset, RAM_TOKEN_CONTRACT);
 }
 
-inline auto operator"" _EOS (unsigned long long value)
-{
-    return eos_asset(eosio::asset(static_cast<int64_t>(value), CORE_SYMBOL));
+static auto operator"" _EOS (unsigned long long value) {
+    return eos_asset(eosio::asset(static_cast<int64_t>(value), EOS_SYMBOL));
 }
 
-inline auto operator"" _RAM (unsigned long long value)
-{
+static auto operator"" _RAM (unsigned long long value) {
     return ram_asset(eosio::asset(static_cast<int64_t>(value), RAM_SYMBOL));
 }
