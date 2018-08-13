284cf310a07ab4360610b3447e556a4c42528f69
smlu <smlu@s5.net>
2018-08-14 01:27:26 +0200

    Add macro DEBUG_ASSERT

--- log.hpp
+++ log.hpp
@@ -5,6 +5,8 @@
 #ifdef DEBUG
 #include <eosiolib/eosio.hpp>
 #define LOG_DEBUG(...) eosio::print_f(__VA_ARGS__); eosio::print("\n")
+#define DEBUG_ASSERT(...) ::eosio_assert(__VA_ARGS__)
 #else
 #define LOG_DEBUG(...)
+#define DEBUG_ASSERT(...)
 #endif
