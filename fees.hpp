adb69f5341a8764d2335e57d129b90485d761bde
smlu <smlu@s5.net>
2018-08-17 22:55:43 +0200

    Add ram market, issue/burn and token transfer fee

--- fees.hpp
+++ fees.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #include <eosiolib/asset.hpp>
-#include <eosiolib/core_symbol.hpp>
 #include <algorithm>
 
 #include "constants.hpp"
+#include "ds/ram_market.hpp"
 
 namespace eosram {
     using namespace eosio;
 
-    template<typename Lambda>
+    constexpr auto buy_fee = [](const asset& amount) -> asset {
     static auto deduct_fee(asset amount, Lambda&& fee_f) -> 
         std::pair</*deducted_amnt=*/asset, /*fee=*/asset>
     {
@@ -16,25 +16,58 @@ namespace eosram {
         amount -= fee;
         return { amount, fee };
     }
-
-    constexpr auto buy_fee = [](const asset& amount) {
         auto fee = amount;
-        fee.amount = std::max((fee.amount + 999) / 1000, 1ll); // 0.1% fee (rounded up)
+        fee.amount = std::max((fee.amount + 999) / 1000LL, 1LL); // 0.1% fee (rounded up)
         return  fee;
     };
 
-    constexpr auto sell_fee = [](const asset& a) {
+    constexpr auto sell_fee = [](const asset& a) -> asset {
         return buy_fee(a);
     };
 
-    constexpr auto cancel_fee = [](const asset& amount) {
+    constexpr auto issue_token_fee = [](const asset& a) -> asset {
+        asset fee = a;
+        fee.amount = 0;
+        return fee;
+    };
+
+    constexpr auto burn_token_fee = [](const asset& a) -> asset {
+        return issue_token_fee(a);
+    };
+
+    constexpr auto token_transfer_fee = [](const asset& amount) -> asset 
+    {
+        constexpr const int64_t transfer_fee_in_ram = 240; 
+        auto fee = amount;
+        if(fee.symbol == EOS_SYMBOL) 
+        {
+            ds::ram_market rm;
+            fee = rm.convert_to_eos(asset(transfer_fee_in_ram, RAM_SYMBOL));
+        }
+        else {
+            fee.amount = transfer_fee_in_ram;
+        }
+
+        return fee;
+    };
+
+    constexpr auto cancel_order_fee = [](const asset& amount) -> asset {
         auto fee = asset(1000, EOS_SYMBOL);
         return  fee;
     };
 
-    constexpr auto expedite_cancel_fee = [](const asset& amount) {
+    constexpr auto expedite_cancel_order_fee = [](const asset& amount) -> asset {
         auto fee = asset(10000, EOS_SYMBOL);
         return  fee;
+    };
+
+
+    // System contract ram market fee
+    // src: https://github.com/EOSIO/eos/blob/d7a6fd10ac0ab34595068f339f8b3d24a06f1db3/contracts/eosio.system/delegate_bandwidth.cpp#L111
+    constexpr auto ram_market_fee = [](const asset& amount) -> asset {
+        asset fee = amount;
+        fee.amount = (fee.amount + 199 ) / 200; /// .5% fee (round up)
+        return fee;
     };
 }
 
